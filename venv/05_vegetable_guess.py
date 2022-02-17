9b6400ed528d2e5a6e95f99310b6931fbbd623ee
magno.franc <magno.franc@awatapu.school.nz>
2022-02-17 14:52:41 +1300

    i think i finished

---
+++ venv/05_vegetable_guess.py
@@ -0,0 +1,20 @@
+print("Welcome to Vegetable Guess!")
+print()
+print("Choose a vegetable as your choice and we will try and guess it!")
+print("Carrot, Peas, Broccoli, Sweetcorn")
+print()
+
+guess_one = input("Is your vegetable green? Y/N").lower()
+if guess_one == "y" or guess_one == "yes":
+    guess_two = input("Is your vegetable shaped like a tree?").lower()
+    if guess_two == "y" or guess_two == "yes":
+        print("It must be a Broccoli!")
+    else:
+        print("It must be Peas!")
+
+else:
+    guess_three = input("Is your vegetable yellow?").lower()
+    if guess_three == "y" or guess_three == "yes":
+        print("It must be a Sweetcorn!")
+    else:
+        print("It must be a Carrot!")
