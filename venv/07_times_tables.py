11e4958fba25e15ccf662040132623f4eacef94d
magno.franc <magno.franc@awatapu.school.nz>
2022-02-28 10:25:25 +1300

    changed according to handout

--- venv/07_times_tables.py
+++ venv/07_times_tables.py
@@ -1,10 +1,18 @@
 
 answer = 0
+start = False
 
-times_tables = int(input("Which times table would you like to see?"))
-print()
+while start != True:
+    try:
+        times_tables = int(input("Which times table would you like to see?"))
+        print()
+        start = True
+        print("Here is the {} times tables".format(times_tables))
+        for x in range(1, 13):
+            answer = x * times_tables
+            print("{} times {} is {}".format(x, times_tables, answer))
 
-print("Here is the {} times tables".format(times_tables))
-for x in range(1, 13):
-    answer = x * times_tables
-    print("{} times {} is {}".format(x, times_tables, answer))
+    except ValueError:
+        print("Please enter a NUMBER!")
+
+
